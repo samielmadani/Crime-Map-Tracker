89a0eb0950d2ac5f02991973e030c1fee4d6ae60
jdm183 <jdm183@uclive.ac.nz>
2021-08-06 11:15:12 +1200

    Created test case and tested it through Maven

--- src/main/java/seng202/group7/App.java
+++ src/main/java/seng202/group7/App.java
@@ -10,4 +10,13 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    public static int power(int number1, int number2) {
+        int result = 1;
+        for (int i = 0; i < number2; i++) {
+            result *= number1;
+        }
+
+        return result;
+    }
 }
