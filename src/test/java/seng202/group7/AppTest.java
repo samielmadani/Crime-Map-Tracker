64a761f98218368a8229aaa64f1e6bb93fc1e024
jdm183 <jdm183@uclive.ac.nz>
2021-08-09 09:04:55 +1200

    Added maven surefire plugin for testing to work

--- src/test/java/seng202/group7/AppTest.java
+++ src/test/java/seng202/group7/AppTest.java
@@ -1,8 +1,6 @@
 package seng202.group7;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -14,11 +12,6 @@ public class AppTest {
     @Test
     public void powerTest() {
         assertEquals(9, App.power(3, 2));
-    }
-
-    @Test
-    public void failingTest(){
-        assertTrue(false);
     }
 
     @Test
