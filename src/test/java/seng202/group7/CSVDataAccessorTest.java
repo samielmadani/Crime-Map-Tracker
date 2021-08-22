0e9a9bff957f97da2ca72b6ed8ceed3fdbc4894c
Shaylin <ssi162@uclive.ac.nz>
2021-08-22 17:11:36 +1200

    Added Test for handling commas in fields Added test for handling blank fields Added test for handling blank rows Edited readFileData test to iterate over all reports

--- src/test/java/seng202/group7/CSVDataAccessorTest.java
+++ src/test/java/seng202/group7/CSVDataAccessorTest.java
@@ -1,6 +1,7 @@
 package seng202.group7;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 
@@ -8,19 +9,55 @@ import org.junit.jupiter.api.Test;
 
 public class CSVDataAccessorTest {
 
+    //TODO: create test naming convention
+
     @Test
-    public void readFileData() {
+    public void isCrimeInstance() {
         CSVDataAccessor dataAccessor = new CSVDataAccessor();
         ArrayList<Report> data = dataAccessor.read("src/crimeData.csv");
-        assertEquals(Crime.class, data.get(0).getClass());
+
+        for(Report report : data){
+            assertTrue(report instanceof Crime);
+        }
     }
 
     @Test
-    public void readFileAmount() {
+    public void loadsAllRows() {
         CSVDataAccessor dataAccessor = new CSVDataAccessor();
         ArrayList<Report> data = dataAccessor.read("src/crimeData.csv");
+
         assertEquals(5000, data.size(), String.format("Not all data was imported correctly." +
         " %d out of 5000 were imported.", data.size()));
         
+    }
+
+    @Test
+    public void handlesCommaInField() {
+        CSVDataAccessor dataAccessor = new CSVDataAccessor();
+        ArrayList<Report> data = dataAccessor.read("src/commaInFieldTestData.csv");
+
+        Report actualReport = data.get(0);
+        Report expectedReport = new Crime("JD389017", CSVDataAccessor.getLocalDateTime("10/05/2020 03:31:00 AM"), "006XX W HARRISON ST", "0454", "BATTERY", "AGGRAVATED P.O. - HANDS, FISTS, FEET, NO / MINOR INJURY", "OTHER COMMERCIAL TRANSPORTATION", true, false, 124, 25, "08B", 1172257, 1897564, 41.874363279, -87.643013039);
+        assertEquals(expectedReport, actualReport);
+    }
+
+    @Test
+    public void handlesBlankField() {
+        CSVDataAccessor dataAccessor = new CSVDataAccessor();
+        ArrayList<Report> data = dataAccessor.read("src/blankFieldTestData.csv");
+
+        Report actualReport = data.get(0);
+        Report expectedReport = new Crime("JE163990", CSVDataAccessor.getLocalDateTime("11/23/2020 03:05:00 PM"), "073XX S SOUTH SHORE DR", "820", "THEFT", "$500 AND UNDER", "APARTMENT", false, false, 334, 7, "6", null, null, null, null);
+        assertEquals(expectedReport, actualReport);
+    }
+
+    @Test
+    public void handlesBlankRow() {
+        CSVDataAccessor dataAccessor = new CSVDataAccessor();
+        ArrayList<Report> data = dataAccessor.read("src/blankRowTestData.csv");
+
+        Report actualReport = data.get(0);
+        Report expectedReport = new Crime(null, null, null, null, null, null, null, false, null, -1, -1, null, null, null, null, null);
+        assertEquals(expectedReport, actualReport);
     }
 }
