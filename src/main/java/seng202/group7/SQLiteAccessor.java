724f2cb5f76e05caadefc9cb8d735cb87bfc6f33
Shaylin <ssi162@uclive.ac.nz>
2021-09-12 04:49:17 +1200

    Inherits from DataAccessor Now conforms to design pattern of CSVDataAccessor Implemented read method

--- src/main/java/seng202/group7/SQLiteAccessor.java
+++ src/main/java/seng202/group7/SQLiteAccessor.java
@@ -1,43 +1,81 @@
 package seng202.group7;
 
+import java.io.File;
 import java.sql.*;
+import java.util.ArrayList;
 
-public class SQLiteAccessor {
-    public static Connection connect() {
-        Connection conn = null;
+public class SQLiteAccessor implements DataAccessor {
+    /**
+     * This creates a singleton instants of the class.
+     */
+    private final static SQLiteAccessor INSTANCE = new SQLiteAccessor();
+
+    /**
+     * The constructor which is made private so that it can not be initialized from other classes.
+     */
+    private SQLiteAccessor() {}
+
+    /**
+     * Used to get the singleton instants of the class when assessing. This is done over a "static" class due to it
+     * implementing an interface.
+     *
+     * @return INSTANCE     The only instants of this class.
+     */
+    public static SQLiteAccessor getInstance() {
+        return INSTANCE;
+    }
+
+
+    public static Connection connect(String url) {
+        Connection connection = null;
         try {
-            // db parameters
-            String url = "jdbc:sqlite:src/main/resources/test.sqlite";
-            // create a connection to the database
-            conn = DriverManager.getConnection(url);
-
-            System.out.println("Connection to SQLite has been established.");
-
+            connection = DriverManager.getConnection(url);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-        return conn;
+        return connection;
     }
 
-    public static void selectAll(){
-        String sql = "SELECT id, \"primary\", arrest FROM tbl_test";
+    @Override
+    public ArrayList<Report> read(File pathname) {
+        Connection connection = connect("jdbc:sqlite:" + pathname);
 
-        try (Connection conn = connect();
-             Statement stmt  = conn.createStatement();
-             ResultSet rs    = stmt.executeQuery(sql)){
-
-            // loop through the result set
+        ArrayList<Report> reports = new ArrayList<>();
+        String query = "SELECT * from crimedb";
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
             while (rs.next()) {
-                System.out.println(rs.getInt("id") +  "\t" +
-                        rs.getString("primary") + "\t" +
-                        rs.getDouble("arrest"));
+                Crime crime = new Crime(
+                        rs.getString("id"),
+                        rs.getTimestamp("date").toLocalDateTime(),
+                        rs.getString("block"),
+                        rs.getString("iucr"),
+                        rs.getString("primary_description"),
+                        rs.getString("secondary_description"),
+                        rs.getString("location_description"),
+                        rs.getBoolean("arrest"),
+                        rs.getBoolean("domestic"),
+                        rs.getInt("beat"),
+                        rs.getInt("ward"),
+                        rs.getString("fbicd"),
+                        rs.getInt("x_coord"),
+                        rs.getInt("y_coord"),
+                        rs.getDouble("latitude"),
+                        rs.getDouble("longitude")
+                );
+                reports.add(crime);
             }
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+        return reports;
     }
 
-    public static void main(String[] args) {
-        selectAll();
+    @Override
+    public void write(ArrayList<Report> reports, String pathname) {
+
     }
+
 }
