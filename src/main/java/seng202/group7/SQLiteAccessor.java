04524645ff761b4531f216e30694b239c0115c5f
Shaylin <ssi162@uclive.ac.nz>
2021-09-12 05:33:44 +1200

    Implemented write method Refactored code from read into select method

--- src/main/java/seng202/group7/SQLiteAccessor.java
+++ src/main/java/seng202/group7/SQLiteAccessor.java
@@ -36,12 +36,8 @@ public class SQLiteAccessor implements DataAccessor {
         return connection;
     }
 
-    @Override
-    public ArrayList<Report> read(File pathname) {
-        Connection connection = connect("jdbc:sqlite:" + pathname);
-
+    public ArrayList<Report> select(Connection connection, String query){
         ArrayList<Report> reports = new ArrayList<>();
-        String query = "SELECT * from crimedb";
         try {
             Statement stmt = connection.createStatement();
             ResultSet rs = stmt.executeQuery(query);
@@ -66,7 +62,7 @@ public class SQLiteAccessor implements DataAccessor {
                 );
                 reports.add(crime);
             }
-
+            rs.close();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
@@ -74,8 +70,60 @@ public class SQLiteAccessor implements DataAccessor {
     }
 
     @Override
-    public void write(ArrayList<Report> reports, String pathname) {
+    public ArrayList<Report> read(File pathname) {
+        Connection connection = connect("jdbc:sqlite:" + pathname);
+        String query = "SELECT * FROM crimedb";
+        ArrayList<Report> reports = select(connection, query);
+        return reports;
 
+    }
+
+//    public ArrayList<Report> getFiltered(File pathname) {
+//        Connection connection = connect("jdbc:sqlite:" + pathname);
+//        String query = "SELECT * FROM crimedb";
+//        ArrayList<Report> reports = select(connection, query);
+//        return reports;
+//    }
+
+    //can only insert values into a fresh table
+    @Override
+    public void write(ArrayList<Report> reports, String pathname) {
+        Connection connection = connect("jdbc:sqlite:" + pathname);
+        PreparedStatement ps = null;
+        try {
+            for(Report report : reports) {
+                String id = null;
+                if (report instanceof Crime) {
+                    Crime crime = (Crime) report;
+                    id = crime.getCaseNumber();
+                    ps = connection.prepareStatement("INSERT INTO crimes(case_number, block, iucr, fbicd, arrest, beat, ward) " +
+                            "VALUES (?, ?, ?, ?, ?, ?, ?);");
+                    ps.setString(1, id);
+                    ps.setString(2, crime.getBlock());
+                    ps.setString(3, crime.getIucr());
+                    ps.setString(4, crime.getFbiCD());
+                    ps.setBoolean(5, crime.getArrest());
+                    ps.setInt(6, crime.getBeat());
+                    ps.setInt(7, crime.getWard());
+                }
+                ps = connection.prepareStatement("INSERT INTO reports(report_id, date, primary_description, secondary_description, domestic, x_coord, y_coord, latitude, longitude) " +
+                        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
+                ps.setString(1, "");
+                ps.setTimestamp(2, Timestamp.valueOf(report.getDate()));
+                ps.setString(3, report.getPrimaryDescription());
+                ps.setString(4, report.getSecondaryDescription());
+                ps.setString(5, report.getLocationDescription());
+                ps.setBoolean(6, report.getDomestic());
+                ps.setInt(7, report.getXCoord());
+                ps.setInt(8, report.getYCoord());
+                ps.setDouble(9, report.getLatitude());
+                ps.setDouble(10, report.getLongitude());
+                ps.executeUpdate();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
