1b50b98222c6138df991a8bd5f5394452c11f06d
Shaylin <ssi162@uclive.ac.nz>
2021-09-23 12:47:20 +1200

    added doc string

--- src/main/java/seng202/group7/QueryBuilder.java
+++ src/main/java/seng202/group7/QueryBuilder.java
@@ -4,10 +4,15 @@ import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDate;
 
+/**
+ * builds queries based on parameters and returns them as Strings
+ * @author Shaylin Simadari
+ */
 public class QueryBuilder {
 
     /**
      * builds a SQL query to retrieve data that meets the conditions of the parameters. null parameters are ommited.
+     * all conditions must be meet f
      * @return a String representation of a SQL query
      */
     public static String where(LocalDate date, String primaryDescription, String locationDescription, Integer ward,
@@ -39,14 +44,33 @@ public class QueryBuilder {
         return query.substring(0, query.length()-5) + ";";
     }
 
+    /**
+     *
+     * @param field the name of the column in the database
+     * @param value the value that the filed must be to meet the condition
+     * @return
+     */
     private static String addAndCondition(String field, boolean value) {
         return field + "=" + (value ? 1 : 0) + " AND ";
     }
 
+    /**
+     *
+     * @param field the name of the column in the database
+     * @param value the value that the filed must be to meet the condition
+     * @return
+     */
     private static String addAndCondition(String field, int value) {
         return field + "=" + value + " AND ";
     }
 
+
+    /**
+     *
+     * @param field the name of the column in the database
+     * @param value the value that the filed must be to meet the condition
+     * @return
+     */
     private static String addAndCondition(String field, String value) {
         return field + "=\'" + value + "\' AND ";
     }
