aab1a2a8c56df6ecc60b8c22d1f0811a0e85a025
Shaylin <ssi162@uclive.ac.nz>
2021-10-01 05:24:03 +1300

    Created Serializable class to store fields that data can be sorted by

---
+++ src/main/java/seng202/group7/data/FilterConditions.java
@@ -0,0 +1,59 @@
+package seng202.group7.data;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class FilterConditions implements Serializable {
+    private LocalDate dateFrom;
+    private LocalDate dateTo;
+    private String primaryDescription;
+    private String locationDescription;
+    private Integer ward;
+    private Integer beat;
+    private Boolean arrest;
+    private Boolean domestic;
+
+    public FilterConditions(LocalDate dateFrom, LocalDate dateTo, String primaryDescription, String locationDescription,
+                            Integer ward, Integer beat, Boolean arrest, Boolean domestic) {
+        this.dateFrom = dateFrom;
+        this.dateTo = dateTo;
+        this.primaryDescription = primaryDescription;
+        this.locationDescription = locationDescription;
+        this.ward = ward;
+        this.beat = beat;
+        this.arrest = arrest;
+        this.domestic = domestic;
+    }
+
+    public LocalDate getDateFrom() {
+        return dateFrom;
+    }
+
+    public LocalDate getDateTo() {
+        return dateTo;
+    }
+
+    public String getPrimaryDescription() {
+        return primaryDescription;
+    }
+
+    public String getLocationDescription() {
+        return locationDescription;
+    }
+
+    public Integer getWard() {
+        return ward;
+    }
+
+    public Integer getBeat() {
+        return beat;
+    }
+
+    public Boolean getArrest() {
+        return arrest;
+    }
+
+    public Boolean getDomestic() {
+        return domestic;
+    }
+}
