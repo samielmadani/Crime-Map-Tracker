6014295f787dbdfb83fad8fb4f4060d2f53b9cf2
jdm183 <jdm183@uclive.ac.nz>
2021-10-02 21:03:06 +1300

    Merging paging into master with updated validation

--- src/main/java/seng202/group7/controllers/PageController.java
+++ src/main/java/seng202/group7/controllers/PageController.java
@@ -16,6 +16,8 @@ import java.net.URL;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
+import javax.xml.validation.Validator;
+
 /**
  * This class creates the paginator that then itself generates the tables,
  * that are used to store selections of the database's information.
@@ -49,6 +51,7 @@ public class PageController implements Initializable {
      */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        InputValidator.addValidation(pageField, InputType.INTEGER);
 
         pageFrame.parentProperty().addListener((obs, oldParent, newParent) -> {
             if (newParent != null) {
@@ -103,28 +106,31 @@ public class PageController implements Initializable {
      */
     public void gotoPage() {
         String input = pageField.getText();
-        if (isValid(input)) {
-            pages.setCurrentPageIndex(Integer.parseInt(input) - 1); // Is valid and changes the page.
+        if (InputValidator.validate(pageField) && !input.isEmpty()) {
+            int queryPage = Integer.parseInt(input);
+            if (queryPage > 0 && queryPage < pages.getPageCount()){
+                pages.setCurrentPageIndex(Integer.parseInt(input) - 1); // Is valid and changes the page.
+            }
         }
     }
 
-    /**
-     * Checks the current page limit for the paginator and checks the value given if it is valid or not.
-     *
-     * @param input     The text input.
-     * @return  The result of if the text input is valid.
-     */
-    private boolean isValid(String input) {
-        // TODO change to new validation method
-        boolean valid;
-        int numOfDig = String.valueOf(pages.getPageCount()).length();
-        if (numOfDig == 0) {
-            valid = false;
-        } else if (numOfDig == 1){
-            valid = input.matches("[1-"+pages.getPageCount()+"]");
-        } else {
-            valid = input.matches("[1-9]([0-9]{0,"+(numOfDig-1)+"})");
-        }
-        return valid;
-    }
+    // /**
+    //  * Checks the current page limit for the paginator and checks the value given if it is valid or not.
+    //  *
+    //  * @param input     The text input.
+    //  * @return  The result of if the text input is valid.
+    //  */
+    // private boolean isValid(String input) {
+    //     // TODO change to new validation method
+    //     boolean valid;
+    //     int numOfDig = String.valueOf(pages.getPageCount()).length();
+    //     if (numOfDig == 0) {
+    //         valid = false;
+    //     } else if (numOfDig == 1){
+    //         valid = input.matches("[1-"+pages.getPageCount()+"]");
+    //     } else {
+    //         valid = input.matches("[1-9]([0-9]{0,"+(numOfDig-1)+"})");
+    //     }
+    //     return valid;
+    // }
 }
