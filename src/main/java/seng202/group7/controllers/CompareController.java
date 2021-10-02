223334b4c6f2d48f98aef3e6719b6e9a99bb63c5
Sam <smc397@uclive.ac.nz>
2021-10-02 18:12:45 +1300

    Combined time and location comparison into one feature, added error handling for null lat and long values, added handling for 0 values when the user adds there own data, added handling for when the values are the same, updated the text feedback.

--- src/main/java/seng202/group7/controllers/CompareController.java
+++ src/main/java/seng202/group7/controllers/CompareController.java
@@ -16,6 +16,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import seng202.group7.data.Crime;
 import seng202.group7.data.DataAccessor;
 import seng202.group7.data.Report;
 import seng202.group7.analyses.Comparer;
@@ -50,29 +51,8 @@ public class CompareController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
 
         errorClass = PseudoClass.getPseudoClass("error");
-
-        frame.parentProperty().addListener((obs, oldParent, newParent) -> {
-
-            if (newParent != null) {
-                setType(frame.getPseudoClassStates());
-            }
-
-        });
     }
 
-    /**
-     * Changes the name of the menu to the type of data that is being compared.
-     * @param pseudoClasses     A style class.
-     */
-    public void setType(ObservableSet<PseudoClass> pseudoClasses) {
-        if (pseudoClasses.contains(PseudoClass.getPseudoClass("distance"))) {
-            menuText.setText("Distance Compare");
-            frame.pseudoClassStateChanged(PseudoClass.getPseudoClass("distance"), false);
-        } else if (pseudoClasses.contains(PseudoClass.getPseudoClass("time"))) {
-            menuText.setText("Time Compare");
-            frame.pseudoClassStateChanged(PseudoClass.getPseudoClass("time"), false);
-        }
-    }
 
     /**
      * Gets the current side panel and replaces it with the general menu panel.
@@ -92,19 +72,27 @@ public class CompareController implements Initializable {
     public void compareReports() {
         DataAccessor data = DataAccessor.getInstance();
         resultText.setText("");
+        String resultTextString = "";
 
         Report reportOne = data.getCrime(reportOneText.getText());
         Report reportTwo = data.getCrime(reportTwoText.getText());
         reportOneText.pseudoClassStateChanged(errorClass, reportOne == null);
         reportTwoText.pseudoClassStateChanged(errorClass, reportTwo == null);
+        Crime c1 = (Crime) reportOne;
+        Crime c2 = (Crime) reportTwo;
         if (reportOne == null || reportTwo == null) {
             return;
+        } else {
+            if (c1.getCaseNumber().equals(c2.getCaseNumber())) {
+                resultTextString += "The two crimes are the same value, please select two different values.";
+            } else {
+                resultTextString += "Distance:";
+                resultTextString += compareDistance(reportOne, reportTwo);
+                resultTextString += "Time:";
+                resultTextString += compareTime(reportOne, reportTwo);
+                }
         }
-        if (menuText.getText().contains("Distance")) {
-            compareDistance(reportOne, reportTwo);
-        } else if (menuText.getText().contains("Time")) {
-            compareTime(reportOne, reportTwo);
-        }
+        resultText.setText(resultTextString);
     }
 
     /**
@@ -113,9 +101,17 @@ public class CompareController implements Initializable {
      * @param reportOne     The first report to be compared
      * @param reportTwo     The second report to be compared
      */
-    private void compareDistance(Report reportOne, Report reportTwo) {
+    private String compareDistance(Report reportOne, Report reportTwo) {
         double distance = Comparer.locationDifference(reportOne, reportTwo);
-        resultText.setText(String.format("Crime %s and %s occurred %.2fkm apart.", reportOneText.getText(), reportTwoText.getText(), distance));
+        if (distance == -1) {
+            return String.format("\nCrime %s has no location values.\n", reportOneText.getText());
+        } else if(distance == -2) {
+            return String.format("\nCrime %s has no location values.\n", reportTwoText.getText());
+        } else if(distance == -3) {
+            return String.format("\nCrime %s and Crime %s have no location values.\n", reportOneText.getText(), reportTwoText.getText());
+        } else {
+            return String.format("\nCrime %s and %s occurred %.2fkm apart.\n", reportOneText.getText(), reportTwoText.getText(), distance);
+        }
     }
 
     /**
@@ -124,25 +120,25 @@ public class CompareController implements Initializable {
      * @param reportOne     The first report to be compared
      * @param reportTwo     The second report to be compared
      */
-    private void compareTime(Report reportOne, Report reportTwo) {
+    private String compareTime(Report reportOne, Report reportTwo) {
         ArrayList<Long> time = Comparer.timeDifference(reportOne, reportTwo);
         String timeString = "";
         if (time.get(3) > 0) {
-            timeString += String.format("%d years ", time.get(3));
+            timeString += String.format("%d year(s) ", time.get(3));
         }
         if (time.get(2) > 0) {
-            timeString += String.format("%d days ", time.get(2));
+            timeString += String.format("%d day(s) ", time.get(2));
         }
         if (time.get(1) > 0) {
-            timeString += String.format("%d hours ", time.get(1));
+            timeString += String.format("%d hour(s) ", time.get(1));
         }
         if (time.get(0) > 0) {
-            timeString += String.format("%d minutes ", time.get(0));
+            timeString += String.format("%d minute(s) ", time.get(0));
         }
         if (timeString.equals("")) {
-            resultText.setText(String.format("Crime %s and %s occurred at the same time.", reportOneText.getText(), reportTwoText.getText()));
+            return String.format("\nCrime %s and %s occurred at the same time.", reportOneText.getText(), reportTwoText.getText());
         } else {
-            resultText.setText(String.format("Crime %s and %s occurred %sapart.", reportOneText.getText(), reportTwoText.getText(), timeString));
+            return String.format("\nCrime %s and %s occurred %sapart.", reportOneText.getText(), reportTwoText.getText(), timeString);
         }
     }
 }
