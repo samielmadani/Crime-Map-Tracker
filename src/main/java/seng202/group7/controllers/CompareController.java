12935f59d79905e64bcc7e4950d387d1746d329e
jdm183 <jdm183@uclive.ac.nz>
2021-10-04 19:16:21 +1300

    Modified apperance of compare selector

--- src/main/java/seng202/group7/controllers/CompareController.java
+++ src/main/java/seng202/group7/controllers/CompareController.java
@@ -5,7 +5,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.ResourceBundle;
-
 import javafx.css.PseudoClass;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
